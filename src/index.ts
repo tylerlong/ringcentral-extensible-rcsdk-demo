868f954e60b7b16a005abcde81673f34148fc2aa
Tyler Long <tyler4long@gmail.com>
2020-07-19 19:27:42 -0700

    First try

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('hello world');
+import SDK from '@ringcentral/sdk';
+import RingCentral from '@rc-ex/core';
+import RingCentralExtension from '@rc-ex/rcsdk';
+
+// @ringcentral/sdk
+const sdk = new SDK({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  server: process.env.RINGCENTRAL_SERVER_URL,
+});
+
+(async () => {
+  await sdk.login({
+    username: process.env.RINGCENTRAL_USERNAME,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD,
+  });
+
+  // ringcentral-extensible + rcsdk extension
+  const rc = new RingCentral();
+  const ringCentralExtension = new RingCentralExtension(sdk);
+  await rc.installExtension(ringCentralExtension);
+
+  // API call with @ringcentral/sdk as HTTP engine
+  const extensionInfo = await rc.restapi().account().extension().get();
+  console.log(extensionInfo.extensionNumber);
+})();
